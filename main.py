464098c6ca66b228dd9d32cd973a0796342fe498
Havva nur Ercan <120741433+Hawanurercan@users.noreply.github.com>
2024-08-01 19:37:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-### PythonProject in input-for-if_else-range-print
-
+### NOTES : PythonProject in input-for-if_else-range-print
+#-------------------
 ### 1-	İki sayının (a,b) durumuna göre şu işlemi yapacaktır. a>b ise 2*a+b-3, b>a ise 2*b+a-3, a=b ise a*b yapacaktır. Bu işlemi yapan programı yazınız? italik metin:
 
 a=int(input("bir a sayısı giriniz:"))
@@ -13,7 +13,7 @@ else:
 
 print(islem)
 
-
+#-------------------
 ##2- Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteren programı yazınız?
 
 maas=int(input("maaş:"))
@@ -21,12 +21,14 @@ zam_orani=int(input("zam oranı %"))
 zamli_maas=(((maas*zam_orani)/100)+maas)
 print(zamli_maas)
 
+#-------------------
 ## 3- 100 arası 3 e ve 5 e tam bölünen sayıları yazınız?
 
 for i in range(1,101):
  if(i%15==0):
   print(i)
 
+#-------------------
 #sigma toplam işlemini göstere bir simgedir. Denkleme göre 2 den n kadar sayılarla işlem yapılacaktır) n dışarıdan girilecektir. n’nin değeri kesinlikle i’nin başlangıç değerinden büyük olması gerekmektedir. Bu işlemi yapan programı yazınız?
 
 n=int(input("bir n değeri giriniz:"))
@@ -37,12 +39,12 @@ if (n>2):
 else:
   print("!! n değeri 2 den küçük veya eşittir tekrar giriniz.")
 
-
+#-------------------
 for i in range(1,36):
  islem=35*i
  print("35",i,islem)
 
- # -------------------
+# -------------------
  say=int(input("s gir"))   #input girişi anlatan yorum satırı
 ctop=0
 ttop=0
@@ -55,8 +57,8 @@ for i in range(1,say+1):
     ttop=ttop+i
 
   print("tek say toplamı= {0} çift say toplamı= {1}".format(ttop,ctop))
+ 
 #-------------------
-
 x= int(input("sayı giriniz:"))
 for i in range(1,x+1):
   print(i)
@@ -74,3 +76,33 @@ for i in range(a,b):
   top=top+i
 
 print("top",top)
+
+#-------------------
+vize1=int(input("vize1:"))
+vize2=int(input("vize2:"))
+final1=int(input("fianl1:"))
+final2=int(input("fianl2:"))
+hesap=float(vize1)*0.4+float(vize2)*0.4+float(final1)*0.6+float(final2)*0.6
+if hesap>=90:
+  print("AA")
+elif hesap<=90:
+  print("BB")
+else :
+ print("CC")
+
+#-------------------
+pi=3.14
+r=float(input("r gir:"))
+print=(r)
+cevre=pi*(r*r)
+alan=2*pi*r
+cevre
+alan
+
+#-------------------
+vize=input("vize gir")
+final=input("final")
+ort=float(vize)*0.4+float(final)*0.6
+print(ort)
+
+#-------------------
